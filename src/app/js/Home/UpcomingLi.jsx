5bc2443bc4b398dca742917b9e42ad215a08a244
Yuki Sato <sato.youxi@gmail.com>
2018-09-22 23:04:02 +0200

    added flipped style function

--- src/app/js/Home/UpcomingLi.jsx
+++ src/app/js/Home/UpcomingLi.jsx
@@ -1,28 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Card, CardText, CardBody,
   CardTitle, CardSubtitle, Button, Row, Col
 } from 'reactstrap';
 import { Link } from 'react-router-dom'
 
+class UpcomingLi extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      flipped: null
+    }
+  }
 
-const UpcomingLi = props => {
-  // console.log("UpcomingLi: ", props)
-  return (
-    <Col sm="6" md="4" lg="3">
-      <Link className='card-link' to={`/meetup/${props.id}`}>
-        <Card body className='m-1 p-0'>
-          <CardBody className='card-hover'>
-            <CardTitle>{props.name}</CardTitle>
-            <CardSubtitle>{props.group}</CardSubtitle>
-            <CardText className='secondary-color font-sm'>{props.date} | {props.time}</CardText>
-            <CardText>Venue: {props.venue} <br />{props.description}</CardText>
-            <Button outline color="secondary" size='sm' className=''>Detail</Button>
-          </CardBody>
-        </Card>
-      </Link>
-    </Col>
-  );
-};
+  mouseOut() {
+    this.setState({ flipped: false });
+  }
+
+  mouseOver() {
+    this.setState({ flipped: true });
+  }
+
+  render() {
+    let flipped = ''
+    if (this.state.flipped) {
+      flipped += ' flipped'
+    }
+
+    return (
+      <Col sm="6" md="4" lg="3">
+        <Link className='card-link' to={`/meetup/${this.props.id}`}>
+          <Card body className='m-1 p-0' >
+            <CardBody
+              className={flipped}
+              onMouseOut={() => this.mouseOut()}
+              onMouseOver={() => this.mouseOver()}
+            >
+              <CardTitle>{this.props.name}</CardTitle>
+              <CardSubtitle>{this.props.group}</CardSubtitle>
+              <CardText className='secondary-color font-sm'>{this.props.date} | {this.props.time}</CardText>
+              <CardText>Venue: {this.props.venue} <br />{this.props.description}</CardText>
+              <Button outline color="secondary" size='sm' className=''>Detail</Button>
+            </CardBody>
+          </Card>
+        </Link>
+      </Col>
+    );
+  }
+}
 
 export default UpcomingLi;
+
